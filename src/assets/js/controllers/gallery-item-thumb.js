a5c5d3b8146822a722e2aa611787e393a371665a
Irina Hristova <hristova119@gmail.com>
2018-02-27 15:56:42 +0200

    Added css for gallery

--- src/assets/js/controllers/gallery-item-thumb.js
+++ src/assets/js/controllers/gallery-item-thumb.js
@@ -1,12 +1,17 @@
 const galleryItemThumb = (function() {
   const thumb = function(node) {
     return `
-    <li class="gallery-item" id="${node.name}">
+<div class="box-content">
+<div class="embed-responsive embed-responsive-16by9">
+<li class="gallery-item" id="${node.name}">
 <img class="gallery-item-thumb" src="${node.imgSrc}" 
 alt="${node.desc}">
 <p class="gallery-item-name">${node.name}</p>
 <img class="like" src="./images/likebutton.png" alt="Gimme some lovin">
 </li>
+</div>
+
+</div>
         `;
   };
 
