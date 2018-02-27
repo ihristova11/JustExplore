d5124e598d42d37c31d889afc08c322ed722e0ae
Irina Hristova <hristova119@gmail.com>
2018-02-27 15:17:52 +0200

    Almost done with the gallery view

--- src/assets/js/controllers/gallery-item-thumb.js
+++ src/assets/js/controllers/gallery-item-thumb.js
@@ -1,24 +1,12 @@
 const galleryItemThumb = (function() {
   const thumb = function(node) {
     return `
-    <div>
-        <div class="blog-card">
-        <div class="title-content">
-            <h3>
-                <a href="#">${node.name}</a>
-            </h3>
-            <div class="intro">
-                <a href="#">${node.parentName}</a>
-            </div>
-        </div>
-        <div class="card-info">
-            ${node.desc}
-        </div>
-       
-        <div class="gradient-overlay"></div>
-        <div class="color-overlay"></div>
-    </div>
-</div>
+    <li class="gallery-item" id="${node.name}">
+<img class="gallery-item-thumb" src="${node.imgSrc}" 
+alt="${node.desc}">
+<p class="gallery-item-name">${node.name}</p>
+<img class="like" src="./images/likebutton.png" alt="Gimme some lovin">
+</li>
         `;
   };
 
@@ -28,9 +16,23 @@ const galleryItemThumb = (function() {
 })();
 
 
-// <li class="gallery-item" id="${node.name}">
-//             <img class="gallery-item-thumb" src="${node.imgSrc}" 
-//             alt="${node.desc}">
-//             <p class="gallery-item-name">${node.name}</p>
-//             <img class="like" src="./images/likebutton.png" alt="Gimme some lovin">
-//         </li>
+
+// to be displayed
+// {/* <div>
+// <div class="blog-card">
+// <div class="title-content">
+//     <h3>
+//         <a href="#">${node.name}</a>
+//     </h3>
+//     <div class="intro">
+//         <a href="#">${node.parentName}</a>
+//     </div>
+// </div>
+// <div class="card-info">
+//     ${node.desc}
+// </div>
+
+// <div class="gradient-overlay"></div>
+// <div class="color-overlay"></div>
+// </div>
+// </div> */}
