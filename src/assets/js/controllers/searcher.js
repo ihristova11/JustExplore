09ac6802750e2046f36617459d50fc62529d21ef
orlindraganov <orlindraganov@gmail.com>
2018-02-28 23:00:50 +0200

    Refactored searcher

---
+++ src/assets/js/controllers/searcher.js
@@ -0,0 +1,41 @@
+const searcher = (function(objTree) {
+    const findBeginningWith = function(value) {
+        const results = [];
+
+        function dfs(value, node, results) {
+            if (!node.isHome) {
+                if (node.name.toLowerCase().indexOf(value.toLowerCase()) === 0) {
+                    results.push(node);
+                }
+            }
+
+            if (node.childNodes && node.childNodes.length) {
+                for (var childNode of node.childNodes) {
+                    dfs(value, childNode, results);
+                }
+            }
+
+            return results;
+        }
+    };
+    const findExactMatch = function(value) {
+        function dfs(value, node) {
+            if (!node.isHome) {
+                if (node.name.toLowerCase() === value.toLowerCase) {
+                    return node;
+                }
+            }
+
+            if (node.childNodes && node.childNodes.length) {
+                for (var childNode of node.childNodes) {
+                    return dfs(value, childNode);
+                }
+            }
+        }
+    };
+    
+    return {
+        findBeginningWith,
+        findExactMatch
+    };
+})();
